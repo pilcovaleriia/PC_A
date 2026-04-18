094b183669987e0c0a78677968b331f032975b80
pilcovaleriia <pilcochuy.valeria@gmail.com>
2026-04-18 15:40:09 -0500

    commit

---
+++ src/test/java/com/upc/pc/pilcovaleria/PilcoValeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.pc.pilcovaleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilcoValeriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
